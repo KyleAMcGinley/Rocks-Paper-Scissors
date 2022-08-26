ac85a4780d1ddb2d5aa39446a2bb9c8e0ca39e96
Kyle McGinley <kyleamcginley@gmail.com>
2022-08-26 16:38:45 -0400

    Wrote out my algorithm for first part
    Will be generating r,p,s from computer randomly

---
+++ myScript.js
@@ -0,0 +1,10 @@
+/* Going to play rocks paper scissors against the computer
+   Start by having the computer randomly generate it's choice
+   Have to start with a function that will generate randomly either 'rock', 'paper', or 'scissors'
+   Will have the function randomly generate an integer between 0 and 2.
+   Will create an array with the options 'rock', 'paper', and 'scissors'
+   Will use the randomly generate number in a splice function to return one of the words */
+
+function getComputerChoice() {
+
+}
