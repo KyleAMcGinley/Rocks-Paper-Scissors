406d5d9824445c3a009c7c71f56d12fd7f9771e1
Kyle McGinley <kyleamcginley@gmail.com>
2022-08-28 14:10:49 -0400

    Changed the result -- no longer console logged.
    Made the user selection prompt inside the function instead of outside

--- myScript.js
+++ myScript.js
@@ -8,45 +8,42 @@
   
    var computerSelection = (function getComputerChoice() {
 
-   let randomNumber = Math.floor(Math.random() * 3);
-
-   let myArray = ['Rock', 'Paper', 'Scissors'];
+      let randomNumber = Math.floor(Math.random() * 3);
    
-   return myArray.splice(randomNumber,1).toString();
-})();
-
-
-
-/* Now have to make a function to play a round of the game
-   I have the function that succesfully generates a selection, which will be a parameter (Will try making parameter the function)
-   Need to use if else in the function to give the outcomes for each result. 
-   Thinking of using a switch function for each possible outcome
-   Will try using a bunch of if elses first*/
-
-   var userSelection = prompt('Enter Rock, Paper, or Scissors');
-   
-   function fiveRoundGame(userSelection){
-
-      for (i = 1; i<=5; i++) {   
-   function playTrial(userSelection) {
+      let myArray = ['Rock', 'Paper', 'Scissors'];
       
-      if (userSelection === computerSelection) {
-         return console.log('It is a draw!');
-      } else if (userSelection === 'Rock' &&  computerSelection === 'Paper') {
-         return console.log('You lose! Paper beats rock!');
-      } else if (userSelection ==='Rock' && computerSelection === 'Scissors') {
-         return console.log('You win! Rock beats Scissors!');         
-      } else if (userSelection === 'Paper' && computerSelection === 'Rock') {
-         return console.log('You win! Paper beats rock!');
-      } else if (userSelection === 'Paper' && computerSelection === 'Scissors') {
-         return console.log('You lose! Scissors beats paper!');
-      } else if (userSelection === 'Scissors' && computerSelection === 'Paper') {
-         return console.log('You win! Scissors beats paper!');
-      } else if (userSelection === 'Scissors' && computerSelection === 'Rock') {
-         return console.log('You lose! Rock beats scissors!');
-      }
-   }
-}
-}
+      return myArray.splice(randomNumber,1).toString();
+   })();
+   
+   
+   
+   /* Now have to make a function to play a round of the game
+      I have the function that succesfully generates a selection, which will be a parameter (Will try making parameter the function)
+      Need to use if else in the function to give the outcomes for each result. 
+      Thinking of using a switch function for each possible outcome
+      Will try using a bunch of if elses first*/
+   
+      var userSelection = '';
+        
+      function playTrial(userSelection, computerSelection) {
 
-   playTrial(userSelection);
+         var userSelection = prompt('Enter Rock, Paper, or Scissors');
+         
+         if (userSelection === computerSelection) {
+            return 'It is a draw!';
+         } else if (userSelection === 'Rock' &&  computerSelection === 'Paper') {
+            return 'You lose! Paper beats rock!';
+         } else if (userSelection ==='Rock' && computerSelection === 'Scissors') {
+            return 'You win! Rock beats Scissors!';         
+         } else if (userSelection === 'Paper' && computerSelection === 'Rock') {
+            return 'You win! Paper beats rock!';
+         } else if (userSelection === 'Paper' && computerSelection === 'Scissors') {
+            return 'You lose! Scissors beats paper!';
+         } else if (userSelection === 'Scissors' && computerSelection === 'Paper') {
+            return 'You win! Scissors beats paper!';
+         } else if (userSelection === 'Scissors' && computerSelection === 'Rock') {
+            return 'You lose! Rock beats scissors!';
+         }
+      }
+   
+      console.log(playTrial(userSelection, computerSelection));
